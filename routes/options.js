4cfa37b17e586cc3b432c0c25aa1fca0d39cb4b0
Hannah Voelker <hannahvoelker13@gmail.com>
2016-11-05 17:35:51 -0400

    Created options list with basic formatting

--- routes/options.js
+++ routes/options.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var options_file = fs.readFileSync('data/options.json');
+var options = JSON.parse(options_file);
 
-/* GET users listing. */
+/* GET options listing. */
 router.get('/', function(req, res, next) {
-  res.render('options');
+  res.render('options', options);
 });
 
 module.exports = router;
