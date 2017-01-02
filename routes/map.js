cd6e20750cdcffe4e19b58b712af27138def46d4
Hannah Voelker <hannahvoelker13@gmail.com>
2017-01-01 21:48:21 -0500

    Rendered router on maps page

--- routes/map.js
+++ routes/map.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('map', { title: 'Health Centers Near You' });
+});
+
+module.exports = router;
