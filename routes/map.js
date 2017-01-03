2c30c715620f270345c4cb23528fb95c5f145215
Hannah Voelker <hannahvoelker13@gmail.com>
2017-01-03 15:52:52 -0500

    accidentally deleted files oops

--- routes/map.js
+++ routes/map.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('map', { title: 'Health Centers Near You' });
 });
